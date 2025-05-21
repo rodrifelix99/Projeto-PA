31e70ca723574555c0abe8ec74f6584347c93fe2
miguel <miguel.carrico@gmail.com>
2025-05-21 19:11:17 +0100

    added KDOC basic

--- src/jsonpackage/models/JsonBoolean.kt
+++ src/jsonpackage/models/JsonBoolean.kt
@@ -4,7 +4,7 @@ package jsonpackage.models
  * JSON Boolean
  *
  * This class represents the Json Boolean that Extends Json Element
- *
+ * @param
  */
 
 data class JsonBoolean(val value: Boolean) : JsonElement() {
